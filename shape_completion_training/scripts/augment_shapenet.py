5a71a4dcaca97e91f6c7654d663cd767d6366756
Brad Saund <brad.saund@gmail.com>
2020-11-11 12:20:29 -0500

    Augment all of the tables

--- shape_completion_training/scripts/augment_shapenet.py
+++ shape_completion_training/scripts/augment_shapenet.py
@@ -53,23 +53,23 @@ if __name__ == "__main__":
 
     start_time = datetime.datetime.now()
 
-    # augment_category(sn_path)
-    augment_category(sn_path, shape_ids=["ff5a2e340869e9c45981503fc6dfccb2",
-                                         "ff60e4b29c5cc38fceda3ac62a593e9c",
-                                         "ff9bb9597cac5ef17a50afc9c93f8a50",
-                                         # "ffa71bb0a75ebd7f93ad7cfe5cf8e21f",
-                                         # "ffa875f5e2242d62d13de1e342854aa",
-                                         # "ffb5e48fde2cca54518bdb78540c51ed",
-                                         # "ffb7b155cea1159a3a8e4d3441f2dd18",
-                                         # "ffc2c7813c80d8fd323d6bd8db8de5b",
-                                         # "ffc75a8bdb88c751b7fbcb21c074906c",
-                                         # "ffd45a17e325bfa91933ffef19678834",
-                                         "ffe02f7b3b421ee96cff9b44fdf0517e",
-                                         "ffe1c487f7b9909bfebad4f49b26ec52",
-                                         "ffe2bf44f5d9760b9a8ef44e1d2c5b75",
-                                         "ffe4383cff6d000a3628187d1bb97b92",
-                                         "fff492e352c8cb336240c88cd4684446",
-                                         "fff7f07d1c4042f8a946c24c4f9fb58e"
-                                         ])
+    augment_category(sn_path)
+    # augment_category(sn_path, shape_ids=["ff5a2e340869e9c45981503fc6dfccb2",
+    #                                      "ff60e4b29c5cc38fceda3ac62a593e9c",
+    #                                      "ff9bb9597cac5ef17a50afc9c93f8a50",
+    #                                      # "ffa71bb0a75ebd7f93ad7cfe5cf8e21f",
+    #                                      # "ffa875f5e2242d62d13de1e342854aa",
+    #                                      # "ffb5e48fde2cca54518bdb78540c51ed",
+    #                                      # "ffb7b155cea1159a3a8e4d3441f2dd18",
+    #                                      # "ffc2c7813c80d8fd323d6bd8db8de5b",
+    #                                      # "ffc75a8bdb88c751b7fbcb21c074906c",
+    #                                      # "ffd45a17e325bfa91933ffef19678834",
+    #                                      "ffe02f7b3b421ee96cff9b44fdf0517e",
+    #                                      "ffe1c487f7b9909bfebad4f49b26ec52",
+    #                                      "ffe2bf44f5d9760b9a8ef44e1d2c5b75",
+    #                                      "ffe4383cff6d000a3628187d1bb97b92",
+    #                                      "fff492e352c8cb336240c88cd4684446",
+    #                                      "fff7f07d1c4042f8a946c24c4f9fb58e"
+    #                                      ])
     print("")
     print("Augmenting with {} threads took {} seconds".format(NUM_THREADS, datetime.datetime.now() - start_time))
